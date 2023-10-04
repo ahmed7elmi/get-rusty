aa07c1c152f4765edd28f921ebe36633d06fb898
helmi <ahmed.7elmi@gmail.com>
2023-10-04 23:23:12 +0200

    Making our hello rustian program a little bit more advanced.

--- hello-rustians/src/main.rs
+++ hello-rustians/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello fellow rustian! What's your name?");
+
+    let mut name = String::new();
+    
+    io::stdin().read_line(&mut name).expect("Failed to read your name.");
+
+    let name = name.trim();
+
+    println!("Horraay {}!! You just wrote your first Rust program :D", name);
 }
